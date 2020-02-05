ca387db0e078406528020f95efca1c52a371009c
Aleksandr Maltsev <47543995+mad250793@users.noreply.github.com>
2020-02-05 22:19:37 +0300

    Create lazy-image.js

---
+++ app/js/lazy-image.js
@@ -0,0 +1,55 @@
+require('intersection-observer');
+
+// для работы нужен div с класом lazy и с атрибутом data-src с путём до картинки ЕСЛИ ТЫ ИСПОЛЬЗУЕШЬ КАРТИНКУ ФОНОМ!
+// ЕСЛИ У ТЕБЯ ТЕГ img добавь scr="no-foto.svg" для всех картинок а в атрибут data-src вставь путь от оригинальной картинки!
+
+
+ 
+
+// при появлении нужного селектора в области видимости КАРТИНКА ПОДГРУГАЕТСЯ!
+
+
+
+
+
+//картинка фоном
+
+    function lazy() {
+        const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const lazyImage = entry.target
+                                      
+                    //let bg = lazyImage.getAttribute('data-src');
+                    //lazyImage.style.backgroundImage = "url(" + bg + ")";
+                    
+                }
+            })
+        });
+        const arr = document.querySelectorAll('.lazy')
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            imageObserver.observe(element);
+        }
+    }
+    
+    
+//динамичная замена в пути к катринке  в img src
+function lazyImg() {
+        const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const lazyImage = entry.target
+                    let bg = lazyImage.getAttribute('data-src');
+                    if (bg.length > 0) {
+                        lazyImage.setAttribute('src', bg);
+                    }
+                }
+            })
+        });
+        const arr = document.querySelectorAll('.lazy-img')
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            imageObserver.observe(element);
+        }
+    }
