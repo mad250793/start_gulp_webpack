5b0e98434ff9ca0c0dd5eb61595a3c3aabae53c4
Aleksandr Maltsev <47543995+mad250793@users.noreply.github.com>
2020-02-09 18:18:50 +0300

    Create hasClass.js

---
+++ app/js/hasClass.js
@@ -0,0 +1,13 @@
+// function hasClass(el, cls) {
+//     return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+// }
+
+// if (hasClass(lazyImage, "active")) {
+
+
+// } else {
+
+//     lazyImage.classList.add("active")
+
+
+// }
