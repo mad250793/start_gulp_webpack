ed849c1f88bab502f2ee94b8b708b0c27abf4381
Aleksandr Maltsev <47543995+mad250793@users.noreply.github.com>
2020-05-05 22:33:14 +0300

    Update spliter-intersection-gsap-tpl.js

--- app/js/spliter-intersection-gsap-tpl.js
+++ app/js/spliter-intersection-gsap-tpl.js
@@ -1,30 +1,22 @@
 require('intersection-observer');
-import {gsap} from 'gsap'
-import Splitter from 'split-html-to-chars';
-  const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+import { gsap} from 'gsap'
+
+const imageObserver = new IntersectionObserver((entries, imgObserver) => {
       entries.forEach((entry) => {
           if (entry.isIntersecting) {
+
               const el = entry.target;
 
               let tl = gsap.timeline();
 
-              let els = document.querySelectorAll(".spl-h1");
-              [].forEach.call(els, function(el) {
-                  el.outerHTML = Splitter(el.outerHTML, '<span class="spl-ltr">$</span>');
-              });
+              let letter = el.textContent.split('').map(e => `<span class="spl-ltr">${e}</span>`)
 
-              tl.from('.spl-h1', { opacity: 0 })
+              el.innerHTML = letter.join('')
 
-              tl.staggerFromTo(".spl-ltr", .3, { opacity: 0, }, { opacity: 1 }, "0.03", 1)
+              tl.staggerFromTo(".spl-ltr", .3, { opacity: 0, }, { opacity: 1 }, "0.03")
 
           }
       })
   });
-  const arr = document.querySelectorAll('.spl-h1')
-  for (let i = 0; i < arr.length; i++) {
-      const element = arr[i];
-      imageObserver.observe(element);
-  }
-}
 
-
+  document.querySelectorAll('.spl-h1').forEach(e => imageObserver.observe(e))
