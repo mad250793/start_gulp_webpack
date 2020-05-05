e9adf3a1d2af66808339c6dced133ba748e70f00
Aleksandr Maltsev <47543995+mad250793@users.noreply.github.com>
2020-05-05 22:36:02 +0300

    Update hasClass.js

--- app/js/hasClass.js
+++ app/js/hasClass.js
@@ -1,14 +1,17 @@
-// function hasClass(el, cls) {
-//     return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
-// }
+//=========  1 варик =======
+function hasClass(el, cls) {
+    return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+}
 
-// if (hasClass(lazyImage, "active")) {
-
-
-// } else {
-
-//     lazyImage.classList.add("active")
+if (hasClass(lazyImage, "active")) {
 
 
-// }
+} else {
+
+    lazyImage.classList.add("active")
+
+
+}
+///=======  2 варик ========
 if (el.classList.contains("active") != true) {}
+
