dc1a96894fa60c393f65f1ee25be6d1dcb866538
Aleksandr Maltsev <47543995+mad250793@users.noreply.github.com>
2020-05-05 22:43:11 +0300

    Create 3dCard.js

---
+++ app/js/3dCard.js
@@ -0,0 +1,101 @@
+// модуль превращающий елементы в 3d елементы реагирущие на курсор мыши!
+
+/// =====как юзать ======
+
+//
+
+card3D.big(element', "-15"); /// для большах катрочек
+card3D.small('.list__item', "0"); /// для мелких катрочек
+
+import { gsap, Power4 } from 'gsap'
+
+export function big(elem, degStart) {
+
+    if (elem != null) {
+
+        if (window.innerWidth > 992) {
+
+            let array = document.querySelectorAll(elem);
+            for (let i = 0; i < array.length; i++) {
+                const element = array[i];
+
+
+
+                element.addEventListener('mousemove', card3d)
+
+                element.addEventListener('mouseout', function() {
+                    let tl = gsap.timeline();
+                    tl.to(this, 3, { transformPerspective: 1200, rotateY: degStart, rotateX: "0", ease: "power4.out" })
+                })
+
+
+                function card3d(e) {
+
+                    let widthC = this.clientWidth;
+                    let heightC = this.clientHeight;
+
+                    let nowCursorW = e.offsetX;
+                    let nowCursorH = e.offsetY;
+
+                    let midW = widthC / 2;
+                    let midH = heightC / 2;
+
+                    let WCard = nowCursorW - midW;
+                    let HCard = nowCursorH - midH;
+
+
+                    let tl = gsap.timeline();
+
+                    tl.to(this, 5, { transformPerspective: 1200, rotateY: WCard / 40, rotateX: -HCard / 40, ease: "power4.out" })
+
+
+                }
+
+
+            }
+        }
+    }
+}
+
+export function small(elem, degStart) {
+
+    if (elem != null) {
+    
+        if (window.innerWidth > 992) {
+            let array = document.querySelectorAll(elem);
+            for (let i = 0; i < array.length; i++) {
+                const element = array[i];
+
+                element.addEventListener('mousemove', card3d)
+
+                element.addEventListener('mouseout', function() {
+                    let tl = gsap.timeline();
+                    tl.to(this, 3, { transformPerspective: 200, rotateY: degStart, rotateX: "0", ease: "power4.out" })
+                })
+
+                function card3d(e) {
+
+
+                    let widthC = this.clientWidth;
+                    let heightC = this.clientHeight;
+
+                    let nowCursorW = e.offsetX;
+                    let nowCursorH = e.offsetY;
+
+                    let midW = widthC / 2;
+                    let midH = heightC / 2;
+
+                    let WCard = nowCursorW - midW;
+                    let HCard = nowCursorH - midH;
+
+
+                    let tl = gsap.timeline();
+
+                    tl.to(this, 3, { transformPerspective: 200, rotateY: WCard / 20, rotateX: -HCard / 20, ease: "power4.out" })
+
+                }
+
+            }
+        }
+    }
+}
